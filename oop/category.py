312d0b95983bde09b923ba7520859b3b81ad26c5
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-11-17 20:54:50 +0300

    Добавлены 2 атрибута:для хранения кол-ва категорий и кол-во уникальных продуктов.

--- oop/category.py
+++ oop/category.py
@@ -13,8 +13,14 @@ class Category:
     name: str
     description: str
     goods: List[Product]
+    unique_products: set = set()
+    total_categories: int = 0
 
     def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
         self.goods = []
+        self.total_categories += 1
+
+        for good in self.goods:
+            self.unique_products.add(good)
