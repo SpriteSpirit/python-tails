3994f73ead357bb3efbf159987da9acd2b1eba2e
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-11-17 22:32:41 +0300

    Создан тест для описания товара

--- oop/tests/test_product.py
+++ oop/tests/test_product.py
@@ -11,4 +11,13 @@ def test_product_name(new_product):
     assert new_product.name.istitle()
 
 
+def test_product_description(new_product):
+    """
+    Тестирование описания продукта.
+    Проверяет, что описание продукта не пустое, содержит только буквы и не длиннее 500 символов.
+    """
+    assert new_product.description != ''
+    assert len(new_product.description) <= 500
+
+
 
