1f6bf0db3dd27694c8ed963163fd4c51c2b7c41f
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-11-18 09:02:18 +0300

    Добавлена фикстура для категории

--- oop/tests/conftest.py
+++ oop/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from oop.src.product import Product
+from oop.src.category import Category
 
 
 @pytest.fixture(params=[
@@ -17,3 +18,13 @@ def make_product():
     def _make_product(name="Товар", description="Описание", price=100, quantity=1):
         return Product(name, description, price, quantity)
     return _make_product
+
+
+@pytest.fixture
+def new_category(make_product):
+    products = [
+        make_product(name='Ноутбук', description='Последней модели', price=70_000.00, quantity=5),
+        make_product(name='Смартфон', description='С камерой 100MP', price=50_000.00, quantity=10),
+        make_product(name='Планшет', description='Для рисования', price=30_000.00, quantity=0)
+    ]
+    return Category('Техника', 'Описание категории техники', products)
