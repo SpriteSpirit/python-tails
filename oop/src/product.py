4c2be82b8d9f23eb4d1378980843eb024d1d1a76
Angelina Khalueva <khalueva.angelina@gmail.com>
2024-11-29 19:03:21 +0300

    Внесены правки

--- oop/src/product.py
+++ oop/src/product.py
@@ -17,3 +17,21 @@ class Product:
         self.description = description
         self.price = price if price > 0 else abs(price)
         self.quantity = quantity if quantity > 0 else abs(quantity)
+
+    @classmethod
+    def create_product(cls, name: str, description: str, price: float, quantity: int, products: list = None):
+        """
+        Создает товар и возвращает объект Product.
+        Проверяет наличие такого же товара по имени в списке products.
+        В случае совпадения имен, суммирует количество и выбирает максимальную цену.
+        """
+
+        if products:
+            for product in products:
+                if product.name == name:
+                    product.quantity += quantity
+                    product.price = max(product.price, price)
+                    print(f"Товар '{name}' уже существует. Количество и цена обновлены.")
+                    return product
+
+        return cls(name, description, price, quantity)
