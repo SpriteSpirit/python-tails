2f4cb926e7d0eb8ff61d42905d034d0094ee6c65
Angelina Khalueva <khalueva.angelina@gmail.com>
2024-11-30 12:41:54 +0300

    Реализован класс Трава газонная

---
+++ oop/src/grass.py
@@ -0,0 +1,24 @@
+from oop.src.product import Product
+
+
+class Grass(Product):
+    """
+    Класс травы.
+    """
+    name: str
+    description: str
+    price: float
+    quantity: int
+    country: str
+    germination_period: int
+    color: str
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: int, color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+    def __str__(self):
+        return (f"{super().__str__()} | Страна-производитель: {self.country}, "
+                f"Срок прорастания : {self.germination_period}, Цвет: {self.color}")
